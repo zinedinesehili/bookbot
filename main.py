69060b5444683162970926a43e886a29f1b79aed
zinny <zinedinesehili@gmail.com>
2025-12-14 10:25:07 -0500

    Prints char counts

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, num_characters, sort_dictionary
 
 def get_book_text(filepath):
     text = ""
@@ -8,6 +8,20 @@ def get_book_text(filepath):
 
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    count_words(book_text)
+    word_count = count_words(book_text)
+    character_count = num_characters(book_text)
+    print(character_count)
+    
+    sorted_chars = sort_dictionary(character_count)
+    
+    print("============ BOOKBOT ============\n")
+    print(f"Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+
+    for char in sorted_chars:
+        if char["char"].isalpha():
+            print(f"{char["char"]}: {char["num"]}")
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,24 @@ def count_words(text):
     split_text = text.split()
     print(f"Found {len(split_text)} total words")
 
+def num_characters(text):
+    character_count = {}
+    for char in text:
+        lower = char.lower()
+        if lower in character_count:
+            character_count[lower] += 1
+        else:
+            character_count[lower] = 1
+    return character_count
+
+def sort_on(characters):
+    return characters["num"]
+
+def sort_dictionary(characters):
+    list_of_dicts = []
+    new_dict = {}
+    for char in characters:
+        new_dict = {"char":char, "num":characters[char]}
+        list_of_dicts.append(new_dict)
+    list_of_dicts.sort(reverse=True, key=sort_on)
+    return list_of_dicts
